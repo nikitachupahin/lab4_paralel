afb82ffb2fa14211804785925651ad3d522c0274
Nikita Chupahin <nik123chupahin@gmail.com>
2025-04-08 18:58:11 +0300

    Extended command processing for server and client

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -2,19 +2,75 @@ package org.example;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     private Socket client;
+    private int matrixSize;
+    private int threadsAmount;
 
     public void initializeConnection(String host, int port) throws IOException {
         client = new Socket(host, port);
         System.out.println("Client was initialized");
     }
 
+    public void startUI() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        boolean hasConnection = true;
+
+        while (hasConnection) {
+            System.out.println("Enter command to server:");
+            String command = scanner.nextLine().trim().toLowerCase();
+            switch (command) {
+                case "send threads amount":
+                    sendThreadsAmount(scanner);
+                    break;
+                case "send matrix size":
+                    sendMatrixSize(scanner);
+                    break;
+                case "end":
+                    RequestHandler.sendMessage(client, "end");
+                    hasConnection = false;
+                    break;
+                default:
+                    RequestHandler.sendMessage(client, command);
+                    System.out.println("Response: " + RequestHandler.getMessage(client));
+            }
+        }
+    }
+
+    private void sendThreadsAmount(Scanner scanner) throws IOException {
+        RequestHandler.sendMessage(client, "send threads amount");
+        System.out.println("Enter threads amount: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("You need to enter a number, try again: ");
+            scanner.next();
+        }
+        threadsAmount = scanner.nextInt();
+        scanner.nextLine();
+        RequestHandler.sendIntValue(client, threadsAmount);
+        System.out.println("Response: " + RequestHandler.getMessage(client));
+    }
+
+    private void sendMatrixSize(Scanner scanner) throws IOException {
+        RequestHandler.sendMessage(client, "send matrix size");
+        System.out.println("Enter matrix size: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("You need to enter a number, try again: ");
+            scanner.next();
+        }
+        matrixSize = scanner.nextInt();
+        scanner.nextLine();
+        RequestHandler.sendIntValue(client, matrixSize);
+        System.out.println("Client send matrix size");
+        System.out.println("Response: " + RequestHandler.getMessage(client));
+    }
+
     public static void main(String[] args) {
         Client client = new Client();
         try {
             client.initializeConnection("localhost", 8081);
+            client.startUI();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -7,6 +7,8 @@ import org.example.MatrixRebuilder;
 
 public class Server {
     private ServerSocket serverListener;
+    private int clientNumber = 0;
+    private boolean serverIsRunning = true;
 
     public void initialize(int port) throws IOException {
         serverListener = new ServerSocket(port);
@@ -15,13 +17,47 @@ public class Server {
 
     public void startListening() {
         System.out.println("Server start listening");
-        while (true) {
+        while (serverIsRunning) {
             try {
                 Socket client = serverListener.accept();
                 System.out.println("Server get a client");
+                clientNumber++;
+                new Thread(() -> handleClient(client)).start();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void handleClient(Socket client) {
+        boolean hasConnection = true;
+        int threadsAmount = 0;
+        int matrixSize = 0;
+        System.out.println("Client connected");
+
+        try {
+            while (hasConnection) {
+                String command = RequestHandler.getMessage(client);
+                switch (command) {
+                    case "send threads amount":
+                        threadsAmount = RequestHandler.getUnsignedInt(client, "Can not use a negative threads amount");
+                        break;
+                    case "send matrix size":
+                        matrixSize = RequestHandler.getUnsignedInt(client, "Can not use a negative matrix size");
+                        break;
+                    case "end":
+                        System.out.println("Client requested to end connection.");
+                        client.shutdownInput();
+                        client.shutdownOutput();
+                        client.close();
+                        hasConnection = false;
+                        break;
+                    default:
+                        RequestHandler.sendMessage(client, "Can not understand a command");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
